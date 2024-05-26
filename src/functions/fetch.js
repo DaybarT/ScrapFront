3911a91331222e6222469b7fdc50b914b13ee137
DaybarT <aybar69@gmail.com>
2024-05-26 02:31:38 +0200

    first commit

---
+++ src/functions/fetch.js
@@ -0,0 +1,32 @@
+async function fetchData(method, link, token, bodyData) {
+  try {
+    headers = {
+      'Content-Type': 'application/json'
+    }
+
+    if (token) {
+      headers.Authorization = 'Bearer ' + token
+    }
+
+    const requestBody = bodyData ? JSON.stringify(bodyData) : null
+
+    // Configurando la solicitud con los encabezados
+    const options = {
+      method: method,
+      headers: headers,
+      body: requestBody
+    }
+
+    const response = await fetch(link, options)
+
+    if (!response.ok) {
+      throw new Error('Hubo un problema al hacer la solicitud: ' + response.status)
+    }
+
+    const data = await response.json()
+
+    console.log(data)
+  } catch (error) {
+    console.error('Hubo un problema con la solicitud:', error)
+  }
+}
